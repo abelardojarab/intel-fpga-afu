4861972341d3d3c9f3aca95295daa2024b04bc5c
Deepak Unnikrishnan <dunnikri@altera.com>
2017-08-15 23:16:21 -0800

    [SJ] use release scripts for nlb_400 [rebuilt_gk:adapt:adapt-g-17.1-131.18-sj] 2017/08/16 01:05:04 PDT [rebuilt:adapt-17.1-133-sj] 2017/08/17 05:50:08 PDT
    [git-p4: depot-paths = "//depot/adapt/main/afu/": change = 4854135]

--- nlb_400/sim/run_app.sh
+++ nlb_400/sim/run_app.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
 
-# NLB_MODE_0
+#get exact script path
+SCRIPT_PATH=`readlink -f ${BASH_SOURCE[0]}`
+#get director of script path
+SCRIPT_DIR_PATH="$(dirname $SCRIPT_PATH)"
+
+. $SCRIPT_DIR_PATH/sim_common.sh
 # Run this script from Terminal 2
-
-set -e
-set -v
-
-# Build the software application
-pushd $PWD/../../../../../../sw/opae-0.3.0/samples
-rm hello_fpga
-gcc -g -o hello_fpga hello_fpga.c -L $PWD/../build/lib/ -I $PWD/../build/include/ -luuid -lpthread -lopae-c-ase -std=c99
-
-# setup env variables
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/../build/lib
-export ASE_WORKDIR=$PWD/../ase/work/
-
-# run the application
-./hello_fpga
-
-popd
-
+menu_run_app "$@"
+# nlb requires special build since app source isn't located with afu
+gcc -g -o $app_base/hello_fpga $app_base/hello_fpga.c -L $opae_base/build/fpga_api_src/inst/lib/ -I $opae_base/build/fpga_api_src/inst/include -luuid -lpthread -lopae-c-ase -std=c99
+run_app

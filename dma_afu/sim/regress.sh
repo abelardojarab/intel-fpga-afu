4dd7b1f784f9355c6fc4419bdd7aaaa8ab665ff9
Deepak Unnikrishnan <dunnikri@altera.com>
2017-09-06 17:30:52 -0800

    [SJ] use release scripts for dma regtest [rebuilt_gk:adapt:adapt-g-17.1-147.12-sj] 2017/09/06 18:50:27 PDT [rebuilt:adapt-17.1-148-sj] 2017/09/07 02:50:06 PDT
    [git-p4: depot-paths = "//depot/adapt/main/afu/": change = 4881545]

--- dma_afu/sim/regress.sh
+++ dma_afu/sim/regress.sh
@@ -4,15 +4,10 @@
 SCRIPT_PATH=`readlink -f ${BASH_SOURCE[0]}`
 #get director of script path
 SCRIPT_DIR_PATH="$(dirname $SCRIPT_PATH)"
+
 . $SCRIPT_DIR_PATH/sim_common.sh
 # Run this script from Terminal 2
-
-set -e
-
-menu_run_app "$@"
-
-wait_for_sim_ready
-setup_app_env
-build_app
-# run the executable
-$app_base/fpga_dma_test 1
+menu_regress "$@"
+sh setup_sim.sh -a $afu -b $opae_base -s $sim &
+sh run_app.sh -a $app -b $opae_base
+kill_sim

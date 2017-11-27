f3b63fa06793749f2b232b4871065edd46fdff06
Rauer, Christopher <christopher.rauer@intel.com>
2017-11-27 15:26:07 -0800

    split avmm_ccip_host to avmm_ccip_host_wr and avmm_ccip_host_rd

--- dma_afu/BBB_ccip_avmm/hw/rtl/ccip_avmm_pkg.sv
+++ dma_afu/BBB_ccip_avmm/hw/rtl/ccip_avmm_pkg.sv
@@ -16,7 +16,8 @@ package ccip_avmm_pkg;
 	} t_ccip_avmm_mmio_cmd;
 
 	parameter CCIP_AVMM_REQUESTOR_DATA_WIDTH = 512;
-	parameter CCIP_AVMM_REQUESTOR_ADDR_WIDTH = 49;
+	parameter CCIP_AVMM_REQUESTOR_WR_ADDR_WIDTH = 49;
+	parameter CCIP_AVMM_REQUESTOR_RD_ADDR_WIDTH = 48;
 	parameter CCIP_AVMM_REQUESTOR_BURST_WIDTH = 3;
 	parameter CCIP_AVMM_REQUESTOR_CONTROL_WIDTH = 1;
 	

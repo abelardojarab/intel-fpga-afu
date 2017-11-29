44dcdb5dadc99768cd8816b34a0fa16a86cb8f9b
Southern, Gabriel <gabriel.southern@intel.com>
2017-11-28 22:47:58 -0800

    Fix possible use of NULL pointer
    In fpgaDmaTransferAsync dma_h is checked for NULL but then used
    after the check.  Updated code exits if NULL check fails fixing
    issue:
    
    ---------------------------------------------------------------------------
    20 (Local) /build2/gsouther/projs/intel-fpga-afu/dma_afu/sw/fpga_dma.c:1008 NPD.CHECK.MUST (1:Critical) Analyze
    Pointer 'dma_h' checked for NULL at line 998 will be dereferenced at line 1008.
      * fpga_dma.c:998: 'dma_h' is checked for NULL.
      * fpga_dma.c:998: !dma_h|| !dma_h->fpga_h is true
      * fpga_dma.c:1001: res==FPGA_OK is false
      * fpga_dma.c:1008: 'dma_h' is explicitly dereferenced.
    Current status 'Analyze'

--- dma_afu/sw/fpga_dma.c
+++ dma_afu/sw/fpga_dma.c
@@ -995,8 +995,10 @@ fpga_result fpgaDmaTransferAsync(fpga_dma_handle dma, uint64_t dst, uint64_t src
 fpga_result fpgaDmaClose(fpga_dma_handle dma_h) {
 	fpga_result res = FPGA_OK;
 	int i = 0;
-	if(!dma_h || !dma_h->fpga_h)
+	if(!dma_h || !dma_h->fpga_h) {
 		res = FPGA_INVALID_PARAM;
+		goto out;
+	}
 
 	if(res == FPGA_OK) {
 		for(i=0; i<FPGA_DMA_MAX_BUF; i++) {

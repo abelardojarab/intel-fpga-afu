75d859e01e818651f89f590cb0e7fa4fe9936531
Michael Adler <Michael.Adler@intel.com>
2018-02-15 15:20:54 -0500

    Forgot to fix SW to get AFU UUID from JSON.

--- samples/hello_mem_afu/sw/hello_mem_afu.c
+++ samples/hello_mem_afu/sw/hello_mem_afu.c
@@ -1,3 +1,29 @@
+// Copyright(c) 2017, Intel Corporation
+//
+// Redistribution  and  use  in source  and  binary  forms,  with  or  without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of  source code  must retain the  above copyright notice,
+//   this list of conditions and the following disclaimer.
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+// * Neither the name  of Intel Corporation  nor the names of its contributors
+//   may be used to  endorse or promote  products derived  from this  software
+//   without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,  BUT NOT LIMITED TO,  THE
+// IMPLIED WARRANTIES OF  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMEdesc.  IN NO EVENT  SHALL THE COPYRIGHT OWNER  OR CONTRIBUTORS BE
+// LIABLE  FOR  ANY  DIRECT,  INDIRECT,  INCIDENTAL,  SPECIAL,  EXEMPLARY,  OR
+// CONSEQUENTIAL  DAMAGES  (INCLUDING,  BUT  NOT LIMITED  TO,  PROCUREMENT  OF
+// SUBSTITUTE GOODS OR SERVICES;  LOSS OF USE,  DATA, OR PROFITS;  OR BUSINESS
+// INTERRUPTION)  HOWEVER CAUSED  AND ON ANY THEORY  OF LIABILITY,  WHETHER IN
+// CONTRACT,  STRICT LIABILITY,  OR TORT  (INCLUDING NEGLIGENCE  OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,  EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -6,7 +32,10 @@
 #include <uuid/uuid.h>
 #include <opae/fpga.h>
 
-#define AVLMM_CONFIG_AFU_ID      "35F9452B-25C2-434C-93D5-6F8C60DB361C"
+// State from the AFU's JSON file, extracted using OPAE's afu_json_mgr script
+#include "afu_json_info.h"
+
+#define AFU_ID                   AFU_ACCEL_UUID  // Defined in afu_json_info.h
 #define SCRATCH_REG              0X80
 #define AVM_ADDRESS_REG          0x100
 #define AVM_BURSTCOUNT_REG       0x108
@@ -108,8 +137,9 @@ int main(int argc, char *argv[])
       sleep_time.tv_nsec = 1000000;
    }
 
-   if (uuid_parse(AVLMM_CONFIG_AFU_ID, guid) < 0) {
-      fprintf(stderr, "Error parsing guid '%s'\n", AVLMM_CONFIG_AFU_ID);
+   // AFU_ACCEL_UUID defined in afu_json_info.h
+   if (uuid_parse(AFU_ACCEL_UUID, guid) < 0) {
+      fprintf(stderr, "Error parsing guid '%s'\n", AFU_ACCEL_UUID);
       goto out_exit;
    }
 

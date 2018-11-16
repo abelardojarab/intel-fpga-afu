c90e39e7074f6257102b814bfee145a7c42a814a
Kylan Roberson(old_UID 14987) <kroberso@sj-nx18.altera.com>
2018-11-15 16:50:35 -0800

    add qsys pipelining to help S10 timing

--- samples/dma_afu/hw/rtl/qsys/S10/dma_test_system/dma_test_system_inst.vhd
+++ samples/dma_afu/hw/rtl/qsys/S10/dma_test_system/dma_test_system_inst.vhd
@@ -1,35 +1,5 @@
 	component dma_test_system is
 		port (
-			ccip_avmm_mmio_waitrequest           : out std_logic;                                         -- waitrequest
-			ccip_avmm_mmio_readdata              : out std_logic_vector(63 downto 0);                     -- readdata
-			ccip_avmm_mmio_readdatavalid         : out std_logic;                                         -- readdatavalid
-			ccip_avmm_mmio_burstcount            : in  std_logic_vector(0 downto 0)   := (others => 'X'); -- burstcount
-			ccip_avmm_mmio_writedata             : in  std_logic_vector(63 downto 0)  := (others => 'X'); -- writedata
-			ccip_avmm_mmio_address               : in  std_logic_vector(17 downto 0)  := (others => 'X'); -- address
-			ccip_avmm_mmio_write                 : in  std_logic                      := 'X';             -- write
-			ccip_avmm_mmio_read                  : in  std_logic                      := 'X';             -- read
-			ccip_avmm_mmio_byteenable            : in  std_logic_vector(7 downto 0)   := (others => 'X'); -- byteenable
-			ccip_avmm_mmio_debugaccess           : in  std_logic                      := 'X';             -- debugaccess
-			ccip_avmm_requestor_rd_waitrequest   : in  std_logic                      := 'X';             -- waitrequest
-			ccip_avmm_requestor_rd_readdata      : in  std_logic_vector(511 downto 0) := (others => 'X'); -- readdata
-			ccip_avmm_requestor_rd_readdatavalid : in  std_logic                      := 'X';             -- readdatavalid
-			ccip_avmm_requestor_rd_burstcount    : out std_logic_vector(2 downto 0);                      -- burstcount
-			ccip_avmm_requestor_rd_writedata     : out std_logic_vector(511 downto 0);                    -- writedata
-			ccip_avmm_requestor_rd_address       : out std_logic_vector(47 downto 0);                     -- address
-			ccip_avmm_requestor_rd_write         : out std_logic;                                         -- write
-			ccip_avmm_requestor_rd_read          : out std_logic;                                         -- read
-			ccip_avmm_requestor_rd_byteenable    : out std_logic_vector(63 downto 0);                     -- byteenable
-			ccip_avmm_requestor_rd_debugaccess   : out std_logic;                                         -- debugaccess
-			ccip_avmm_requestor_wr_waitrequest   : in  std_logic                      := 'X';             -- waitrequest
-			ccip_avmm_requestor_wr_readdata      : in  std_logic_vector(511 downto 0) := (others => 'X'); -- readdata
-			ccip_avmm_requestor_wr_readdatavalid : in  std_logic                      := 'X';             -- readdatavalid
-			ccip_avmm_requestor_wr_burstcount    : out std_logic_vector(2 downto 0);                      -- burstcount
-			ccip_avmm_requestor_wr_writedata     : out std_logic_vector(511 downto 0);                    -- writedata
-			ccip_avmm_requestor_wr_address       : out std_logic_vector(48 downto 0);                     -- address
-			ccip_avmm_requestor_wr_write         : out std_logic;                                         -- write
-			ccip_avmm_requestor_wr_read          : out std_logic;                                         -- read
-			ccip_avmm_requestor_wr_byteenable    : out std_logic_vector(63 downto 0);                     -- byteenable
-			ccip_avmm_requestor_wr_debugaccess   : out std_logic;                                         -- debugaccess
 			ddr4a_clk_clk                        : in  std_logic                      := 'X';             -- clk
 			ddr4a_master_waitrequest             : in  std_logic                      := 'X';             -- waitrequest
 			ddr4a_master_readdata                : in  std_logic_vector(511 downto 0) := (others => 'X'); -- readdata
@@ -74,44 +44,44 @@
 			ddr4d_master_read                    : out std_logic;                                         -- read
 			ddr4d_master_byteenable              : out std_logic_vector(63 downto 0);                     -- byteenable
 			ddr4d_master_debugaccess             : out std_logic;                                         -- debugaccess
-			dma_irq_irq                          : out std_logic;                                         -- irq
+			ccip_avmm_mmio_waitrequest           : out std_logic;                                         -- waitrequest
+			ccip_avmm_mmio_readdata              : out std_logic_vector(63 downto 0);                     -- readdata
+			ccip_avmm_mmio_readdatavalid         : out std_logic;                                         -- readdatavalid
+			ccip_avmm_mmio_burstcount            : in  std_logic_vector(0 downto 0)   := (others => 'X'); -- burstcount
+			ccip_avmm_mmio_writedata             : in  std_logic_vector(63 downto 0)  := (others => 'X'); -- writedata
+			ccip_avmm_mmio_address               : in  std_logic_vector(17 downto 0)  := (others => 'X'); -- address
+			ccip_avmm_mmio_write                 : in  std_logic                      := 'X';             -- write
+			ccip_avmm_mmio_read                  : in  std_logic                      := 'X';             -- read
+			ccip_avmm_mmio_byteenable            : in  std_logic_vector(7 downto 0)   := (others => 'X'); -- byteenable
+			ccip_avmm_mmio_debugaccess           : in  std_logic                      := 'X';             -- debugaccess
 			host_clk_clk                         : in  std_logic                      := 'X';             -- clk
+			dma_irq_irq                          : out std_logic;                                         -- irq
+			ccip_avmm_requestor_rd_waitrequest   : in  std_logic                      := 'X';             -- waitrequest
+			ccip_avmm_requestor_rd_readdata      : in  std_logic_vector(511 downto 0) := (others => 'X'); -- readdata
+			ccip_avmm_requestor_rd_readdatavalid : in  std_logic                      := 'X';             -- readdatavalid
+			ccip_avmm_requestor_rd_burstcount    : out std_logic_vector(2 downto 0);                      -- burstcount
+			ccip_avmm_requestor_rd_writedata     : out std_logic_vector(511 downto 0);                    -- writedata
+			ccip_avmm_requestor_rd_address       : out std_logic_vector(47 downto 0);                     -- address
+			ccip_avmm_requestor_rd_write         : out std_logic;                                         -- write
+			ccip_avmm_requestor_rd_read          : out std_logic;                                         -- read
+			ccip_avmm_requestor_rd_byteenable    : out std_logic_vector(63 downto 0);                     -- byteenable
+			ccip_avmm_requestor_rd_debugaccess   : out std_logic;                                         -- debugaccess
+			ccip_avmm_requestor_wr_waitrequest   : in  std_logic                      := 'X';             -- waitrequest
+			ccip_avmm_requestor_wr_readdata      : in  std_logic_vector(511 downto 0) := (others => 'X'); -- readdata
+			ccip_avmm_requestor_wr_readdatavalid : in  std_logic                      := 'X';             -- readdatavalid
+			ccip_avmm_requestor_wr_burstcount    : out std_logic_vector(2 downto 0);                      -- burstcount
+			ccip_avmm_requestor_wr_writedata     : out std_logic_vector(511 downto 0);                    -- writedata
+			ccip_avmm_requestor_wr_address       : out std_logic_vector(48 downto 0);                     -- address
+			ccip_avmm_requestor_wr_write         : out std_logic;                                         -- write
+			ccip_avmm_requestor_wr_read          : out std_logic;                                         -- read
+			ccip_avmm_requestor_wr_byteenable    : out std_logic_vector(63 downto 0);                     -- byteenable
+			ccip_avmm_requestor_wr_debugaccess   : out std_logic;                                         -- debugaccess
 			reset_reset                          : in  std_logic                      := 'X'              -- reset
 		);
 	end component dma_test_system;
 
 	u0 : component dma_test_system
 		port map (
-			ccip_avmm_mmio_waitrequest           => CONNECTED_TO_ccip_avmm_mmio_waitrequest,           --         ccip_avmm_mmio.waitrequest
-			ccip_avmm_mmio_readdata              => CONNECTED_TO_ccip_avmm_mmio_readdata,              --                       .readdata
-			ccip_avmm_mmio_readdatavalid         => CONNECTED_TO_ccip_avmm_mmio_readdatavalid,         --                       .readdatavalid
-			ccip_avmm_mmio_burstcount            => CONNECTED_TO_ccip_avmm_mmio_burstcount,            --                       .burstcount
-			ccip_avmm_mmio_writedata             => CONNECTED_TO_ccip_avmm_mmio_writedata,             --                       .writedata
-			ccip_avmm_mmio_address               => CONNECTED_TO_ccip_avmm_mmio_address,               --                       .address
-			ccip_avmm_mmio_write                 => CONNECTED_TO_ccip_avmm_mmio_write,                 --                       .write
-			ccip_avmm_mmio_read                  => CONNECTED_TO_ccip_avmm_mmio_read,                  --                       .read
-			ccip_avmm_mmio_byteenable            => CONNECTED_TO_ccip_avmm_mmio_byteenable,            --                       .byteenable
-			ccip_avmm_mmio_debugaccess           => CONNECTED_TO_ccip_avmm_mmio_debugaccess,           --                       .debugaccess
-			ccip_avmm_requestor_rd_waitrequest   => CONNECTED_TO_ccip_avmm_requestor_rd_waitrequest,   -- ccip_avmm_requestor_rd.waitrequest
-			ccip_avmm_requestor_rd_readdata      => CONNECTED_TO_ccip_avmm_requestor_rd_readdata,      --                       .readdata
-			ccip_avmm_requestor_rd_readdatavalid => CONNECTED_TO_ccip_avmm_requestor_rd_readdatavalid, --                       .readdatavalid
-			ccip_avmm_requestor_rd_burstcount    => CONNECTED_TO_ccip_avmm_requestor_rd_burstcount,    --                       .burstcount
-			ccip_avmm_requestor_rd_writedata     => CONNECTED_TO_ccip_avmm_requestor_rd_writedata,     --                       .writedata
-			ccip_avmm_requestor_rd_address       => CONNECTED_TO_ccip_avmm_requestor_rd_address,       --                       .address
-			ccip_avmm_requestor_rd_write         => CONNECTED_TO_ccip_avmm_requestor_rd_write,         --                       .write
-			ccip_avmm_requestor_rd_read          => CONNECTED_TO_ccip_avmm_requestor_rd_read,          --                       .read
-			ccip_avmm_requestor_rd_byteenable    => CONNECTED_TO_ccip_avmm_requestor_rd_byteenable,    --                       .byteenable
-			ccip_avmm_requestor_rd_debugaccess   => CONNECTED_TO_ccip_avmm_requestor_rd_debugaccess,   --                       .debugaccess
-			ccip_avmm_requestor_wr_waitrequest   => CONNECTED_TO_ccip_avmm_requestor_wr_waitrequest,   -- ccip_avmm_requestor_wr.waitrequest
-			ccip_avmm_requestor_wr_readdata      => CONNECTED_TO_ccip_avmm_requestor_wr_readdata,      --                       .readdata
-			ccip_avmm_requestor_wr_readdatavalid => CONNECTED_TO_ccip_avmm_requestor_wr_readdatavalid, --                       .readdatavalid
-			ccip_avmm_requestor_wr_burstcount    => CONNECTED_TO_ccip_avmm_requestor_wr_burstcount,    --                       .burstcount
-			ccip_avmm_requestor_wr_writedata     => CONNECTED_TO_ccip_avmm_requestor_wr_writedata,     --                       .writedata
-			ccip_avmm_requestor_wr_address       => CONNECTED_TO_ccip_avmm_requestor_wr_address,       --                       .address
-			ccip_avmm_requestor_wr_write         => CONNECTED_TO_ccip_avmm_requestor_wr_write,         --                       .write
-			ccip_avmm_requestor_wr_read          => CONNECTED_TO_ccip_avmm_requestor_wr_read,          --                       .read
-			ccip_avmm_requestor_wr_byteenable    => CONNECTED_TO_ccip_avmm_requestor_wr_byteenable,    --                       .byteenable
-			ccip_avmm_requestor_wr_debugaccess   => CONNECTED_TO_ccip_avmm_requestor_wr_debugaccess,   --                       .debugaccess
 			ddr4a_clk_clk                        => CONNECTED_TO_ddr4a_clk_clk,                        --              ddr4a_clk.clk
 			ddr4a_master_waitrequest             => CONNECTED_TO_ddr4a_master_waitrequest,             --           ddr4a_master.waitrequest
 			ddr4a_master_readdata                => CONNECTED_TO_ddr4a_master_readdata,                --                       .readdata
@@ -156,8 +126,38 @@
 			ddr4d_master_read                    => CONNECTED_TO_ddr4d_master_read,                    --                       .read
 			ddr4d_master_byteenable              => CONNECTED_TO_ddr4d_master_byteenable,              --                       .byteenable
 			ddr4d_master_debugaccess             => CONNECTED_TO_ddr4d_master_debugaccess,             --                       .debugaccess
-			dma_irq_irq                          => CONNECTED_TO_dma_irq_irq,                          --                dma_irq.irq
+			ccip_avmm_mmio_waitrequest           => CONNECTED_TO_ccip_avmm_mmio_waitrequest,           --         ccip_avmm_mmio.waitrequest
+			ccip_avmm_mmio_readdata              => CONNECTED_TO_ccip_avmm_mmio_readdata,              --                       .readdata
+			ccip_avmm_mmio_readdatavalid         => CONNECTED_TO_ccip_avmm_mmio_readdatavalid,         --                       .readdatavalid
+			ccip_avmm_mmio_burstcount            => CONNECTED_TO_ccip_avmm_mmio_burstcount,            --                       .burstcount
+			ccip_avmm_mmio_writedata             => CONNECTED_TO_ccip_avmm_mmio_writedata,             --                       .writedata
+			ccip_avmm_mmio_address               => CONNECTED_TO_ccip_avmm_mmio_address,               --                       .address
+			ccip_avmm_mmio_write                 => CONNECTED_TO_ccip_avmm_mmio_write,                 --                       .write
+			ccip_avmm_mmio_read                  => CONNECTED_TO_ccip_avmm_mmio_read,                  --                       .read
+			ccip_avmm_mmio_byteenable            => CONNECTED_TO_ccip_avmm_mmio_byteenable,            --                       .byteenable
+			ccip_avmm_mmio_debugaccess           => CONNECTED_TO_ccip_avmm_mmio_debugaccess,           --                       .debugaccess
 			host_clk_clk                         => CONNECTED_TO_host_clk_clk,                         --               host_clk.clk
+			dma_irq_irq                          => CONNECTED_TO_dma_irq_irq,                          --                dma_irq.irq
+			ccip_avmm_requestor_rd_waitrequest   => CONNECTED_TO_ccip_avmm_requestor_rd_waitrequest,   -- ccip_avmm_requestor_rd.waitrequest
+			ccip_avmm_requestor_rd_readdata      => CONNECTED_TO_ccip_avmm_requestor_rd_readdata,      --                       .readdata
+			ccip_avmm_requestor_rd_readdatavalid => CONNECTED_TO_ccip_avmm_requestor_rd_readdatavalid, --                       .readdatavalid
+			ccip_avmm_requestor_rd_burstcount    => CONNECTED_TO_ccip_avmm_requestor_rd_burstcount,    --                       .burstcount
+			ccip_avmm_requestor_rd_writedata     => CONNECTED_TO_ccip_avmm_requestor_rd_writedata,     --                       .writedata
+			ccip_avmm_requestor_rd_address       => CONNECTED_TO_ccip_avmm_requestor_rd_address,       --                       .address
+			ccip_avmm_requestor_rd_write         => CONNECTED_TO_ccip_avmm_requestor_rd_write,         --                       .write
+			ccip_avmm_requestor_rd_read          => CONNECTED_TO_ccip_avmm_requestor_rd_read,          --                       .read
+			ccip_avmm_requestor_rd_byteenable    => CONNECTED_TO_ccip_avmm_requestor_rd_byteenable,    --                       .byteenable
+			ccip_avmm_requestor_rd_debugaccess   => CONNECTED_TO_ccip_avmm_requestor_rd_debugaccess,   --                       .debugaccess
+			ccip_avmm_requestor_wr_waitrequest   => CONNECTED_TO_ccip_avmm_requestor_wr_waitrequest,   -- ccip_avmm_requestor_wr.waitrequest
+			ccip_avmm_requestor_wr_readdata      => CONNECTED_TO_ccip_avmm_requestor_wr_readdata,      --                       .readdata
+			ccip_avmm_requestor_wr_readdatavalid => CONNECTED_TO_ccip_avmm_requestor_wr_readdatavalid, --                       .readdatavalid
+			ccip_avmm_requestor_wr_burstcount    => CONNECTED_TO_ccip_avmm_requestor_wr_burstcount,    --                       .burstcount
+			ccip_avmm_requestor_wr_writedata     => CONNECTED_TO_ccip_avmm_requestor_wr_writedata,     --                       .writedata
+			ccip_avmm_requestor_wr_address       => CONNECTED_TO_ccip_avmm_requestor_wr_address,       --                       .address
+			ccip_avmm_requestor_wr_write         => CONNECTED_TO_ccip_avmm_requestor_wr_write,         --                       .write
+			ccip_avmm_requestor_wr_read          => CONNECTED_TO_ccip_avmm_requestor_wr_read,          --                       .read
+			ccip_avmm_requestor_wr_byteenable    => CONNECTED_TO_ccip_avmm_requestor_wr_byteenable,    --                       .byteenable
+			ccip_avmm_requestor_wr_debugaccess   => CONNECTED_TO_ccip_avmm_requestor_wr_debugaccess,   --                       .debugaccess
 			reset_reset                          => CONNECTED_TO_reset_reset                           --                  reset.reset
 		);
 
